34942c7e05a5633da40a44c7b10340e8429ab607
Toto-Dorset <26043@student.dorset-college.ie>
2022-11-24 11:42:57 +0000

    add differents buttons and panels

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,167 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PanelBottom = new System.Windows.Forms.Panel();
+            this.PanelLeft = new System.Windows.Forms.Panel();
+            this.PanelPlaylist = new System.Windows.Forms.Panel();
+            this.FavoritesButton = new System.Windows.Forms.Button();
+            this.PlaylistButton = new System.Windows.Forms.Button();
+            this.LibraryButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.icbtn = new System.Windows.Forms.Button();
-            this.icbtnmusic = new System.Windows.Forms.Button();
-            this.icbtnmedia = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
+            this.LibraryPanel = new System.Windows.Forms.Panel();
+            this.FavoritesPanel = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PanelLeft.SuspendLayout();
+            this.PanelPlaylist.SuspendLayout();
+            this.LibraryPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // PanelBottom
+            // 
+            this.PanelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PanelBottom.Location = new System.Drawing.Point(0, 651);
+            this.PanelBottom.Name = "PanelBottom";
+            this.PanelBottom.Size = new System.Drawing.Size(1357, 69);
+            this.PanelBottom.TabIndex = 0;
+            // 
+            // PanelLeft
+            // 
+            this.PanelLeft.Controls.Add(this.PanelPlaylist);
+            this.PanelLeft.Controls.Add(this.PlaylistButton);
+            this.PanelLeft.Controls.Add(this.LibraryButton);
+            this.PanelLeft.Controls.Add(this.panel1);
+            this.PanelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelLeft.Location = new System.Drawing.Point(0, 0);
+            this.PanelLeft.Name = "PanelLeft";
+            this.PanelLeft.Size = new System.Drawing.Size(229, 651);
+            this.PanelLeft.TabIndex = 1;
+            // 
+            // PanelPlaylist
+            // 
+            this.PanelPlaylist.Controls.Add(this.FavoritesButton);
+            this.PanelPlaylist.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelPlaylist.Location = new System.Drawing.Point(0, 192);
+            this.PanelPlaylist.Name = "PanelPlaylist";
+            this.PanelPlaylist.Size = new System.Drawing.Size(229, 459);
+            this.PanelPlaylist.TabIndex = 0;
+            // 
+            // FavoritesButton
+            // 
+            this.FavoritesButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.FavoritesButton.Location = new System.Drawing.Point(0, 0);
+            this.FavoritesButton.Name = "FavoritesButton";
+            this.FavoritesButton.Size = new System.Drawing.Size(229, 64);
+            this.FavoritesButton.TabIndex = 0;
+            this.FavoritesButton.Text = "Favorites";
+            this.FavoritesButton.UseVisualStyleBackColor = true;
+            this.FavoritesButton.Click += new System.EventHandler(this.FavoritesButton_Click);
+            // 
+            // PlaylistButton
+            // 
+            this.PlaylistButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PlaylistButton.Location = new System.Drawing.Point(0, 130);
+            this.PlaylistButton.Name = "PlaylistButton";
+            this.PlaylistButton.Size = new System.Drawing.Size(229, 62);
+            this.PlaylistButton.TabIndex = 3;
+            this.PlaylistButton.Text = "Playlist";
+            this.PlaylistButton.UseVisualStyleBackColor = true;
+            this.PlaylistButton.Click += new System.EventHandler(this.PlaylistButton_Click);
+            // 
+            // LibraryButton
+            // 
+            this.LibraryButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LibraryButton.Location = new System.Drawing.Point(0, 68);
+            this.LibraryButton.Name = "LibraryButton";
+            this.LibraryButton.Size = new System.Drawing.Size(229, 62);
+            this.LibraryButton.TabIndex = 2;
+            this.LibraryButton.Text = "Library";
+            this.LibraryButton.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(262, 576);
+            this.panel1.Size = new System.Drawing.Size(229, 68);
             this.panel1.TabIndex = 0;
             // 
-            // panel3
+            // LibraryPanel
             // 
-            this.panel3.Controls.Add(this.button4);
-            this.panel3.Controls.Add(this.button3);
-            this.panel3.Controls.Add(this.button2);
-            this.panel3.Controls.Add(this.button1);
-            this.panel3.Controls.Add(this.panel4);
-            this.panel3.Controls.Add(this.icbtnmedia);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 100);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(262, 483);
-            this.panel3.TabIndex = 1;
+            this.LibraryPanel.Controls.Add(this.textBox1);
+            this.LibraryPanel.Controls.Add(this.FavoritesPanel);
+            this.LibraryPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LibraryPanel.Location = new System.Drawing.Point(229, 0);
+            this.LibraryPanel.Name = "LibraryPanel";
+            this.LibraryPanel.Size = new System.Drawing.Size(1128, 651);
+            this.LibraryPanel.TabIndex = 2;
             // 
-            // panel4
+            // FavoritesPanel
             // 
-            this.panel4.Controls.Add(this.icbtn);
-            this.panel4.Controls.Add(this.icbtnmusic);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(0, 64);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(262, 133);
-            this.panel4.TabIndex = 1;
-            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            this.FavoritesPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FavoritesPanel.Location = new System.Drawing.Point(0, 0);
+            this.FavoritesPanel.Name = "FavoritesPanel";
+            this.FavoritesPanel.Size = new System.Drawing.Size(1128, 651);
+            this.FavoritesPanel.TabIndex = 1;
             // 
-            // icbtn
+            // textBox1
             // 
-            this.icbtn.Dock = System.Windows.Forms.DockStyle.Top;
-            this.icbtn.Location = new System.Drawing.Point(0, 63);
-            this.icbtn.Name = "icbtn";
-            this.icbtn.Size = new System.Drawing.Size(262, 64);
-            this.icbtn.TabIndex = 1;
-            this.icbtn.Text = "button3";
-            this.icbtn.UseVisualStyleBackColor = true;
-            this.icbtn.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // icbtnmusic
-            // 
-            this.icbtnmusic.Dock = System.Windows.Forms.DockStyle.Top;
-            this.icbtnmusic.Location = new System.Drawing.Point(0, 0);
-            this.icbtnmusic.Name = "icbtnmusic";
-            this.icbtnmusic.Size = new System.Drawing.Size(262, 63);
-            this.icbtnmusic.TabIndex = 0;
-            this.icbtnmusic.Text = "Open music";
-            this.icbtnmusic.UseVisualStyleBackColor = true;
-            this.icbtnmusic.Click += new System.EventHandler(this.icbtnmusic_Click);
-            // 
-            // icbtnmedia
-            // 
-            this.icbtnmedia.Dock = System.Windows.Forms.DockStyle.Top;
-            this.icbtnmedia.Location = new System.Drawing.Point(0, 0);
-            this.icbtnmedia.Name = "icbtnmedia";
-            this.icbtnmedia.Size = new System.Drawing.Size(262, 64);
-            this.icbtnmedia.TabIndex = 0;
-            this.icbtnmedia.Text = "Media";
-            this.icbtnmedia.UseVisualStyleBackColor = true;
-            this.icbtnmedia.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(262, 100);
-            this.panel2.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.Location = new System.Drawing.Point(0, 197);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(262, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.Location = new System.Drawing.Point(0, 220);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(262, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button3.Location = new System.Drawing.Point(0, 243);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(262, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button4.Location = new System.Drawing.Point(0, 266);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(262, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.textBox1.Location = new System.Drawing.Point(354, 269);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(395, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Library";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1206, 576);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1357, 720);
+            this.Controls.Add(this.LibraryPanel);
+            this.Controls.Add(this.PanelLeft);
+            this.Controls.Add(this.PanelBottom);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelLeft.ResumeLayout(false);
+            this.PanelPlaylist.ResumeLayout(false);
+            this.LibraryPanel.ResumeLayout(false);
+            this.LibraryPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel PanelBottom;
+        private System.Windows.Forms.Panel PanelLeft;
+        private System.Windows.Forms.Button PlaylistButton;
+        private System.Windows.Forms.Button LibraryButton;
+        private System.Windows.Forms.Panel PanelPlaylist;
+        private System.Windows.Forms.Button FavoritesButton;
+        private System.Windows.Forms.Panel LibraryPanel;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button icbtnmedia;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Button icbtn;
-        private System.Windows.Forms.Button icbtnmusic;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel FavoritesPanel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace MusicPlayer
 {
     public partial class Form1 : Form
     {
+
+        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+        string s;
         public Form1()
         {
             InitializeComponent();
@@ -23,32 +27,90 @@ namespace MusicPlayer
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void PlaylistButton_Click(object sender, EventArgs e)
         {
             presently();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            hide();
         }
 
         private void hide()
         {
-            panel4.Visible = false;
+            PanelPlaylist.Visible = false;
         }
 
         private void presently()
         {
-            if (panel4.Visible == false)
+            if (PanelPlaylist.Visible == false)
             {
-                panel4.Visible = true;
+                PanelPlaylist.Visible = true;
             }
+        }
+
+        private void runmp3()
+        {
+            player.URL = s;
+            player.controls.play();
         }
 
         private void icbtnmusic_Click(object sender, EventArgs e)
         {
+            hide();
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                    s = files[0];
+                    runmp3();
+                }
+            }
+
+            //OpenFileDialog dlg = new OpenFileDialog();
+            //if(dlg.ShowDialog()==DialogResult.OK)
+            //{
+            //    s = dlg.FileName;
+            //    runmp3();
+            //}
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
 
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(player.playState== WMPLib.WMPPlayState.wmppsPlaying)
+            {
+
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FavoritesButton_Click(object sender, EventArgs e)
+        {
+            LibraryPanel.Visible = false;
+            FavoritesPanel.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LibraryPanel.Visible = true;
+            FavoritesPanel.Visible = false;
+        }
+
+        
     }
 }
